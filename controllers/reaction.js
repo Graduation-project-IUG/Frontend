bea151b600c438c9df6b587a148828dcfc217fd5
SaeedAdas <adasssaeed@gmail.com>
2026-05-24 09:44:44 +0300

    Adding report and reactino controllers

---
+++ controllers/reaction.js
@@ -0,0 +1,77 @@
+const prisma = require("../config/connection");
+const messages = require("../helper/messages");
+
+	userId    Int       @map("user_id")
+	postId    Int       @map("post_id")
+	reaction  Int
+const create = async (req, res) => {
+	try {
+		const post_id = req.params.post_id;	
+		const user_id = req.session.user_id;
+		const { reaction } = req.body;	
+
+		const reaction = await prisma.reaction.create({
+			data: {
+				postId: post_id,
+				userId: user_id,
+				reaction, 
+			}
+		});
+
+		return messages.createdSuccessfully(res, "Reaction Saved Successfully");
+
+	} catch (error) {
+		console.error("Saving reaction error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+const retrieve = async (req, res) => {
+	try {
+		const reaction = req.data;
+
+		res.json(reaction);
+
+	} catch (error) {
+		console.error("Retrieving reaction error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+const update = async (req, res) => {
+	try {
+
+		return messages.notImplemented(res);
+
+		//await prisma.reaction.update();
+		
+	} catch (error) {
+		console.error("Updating reaction error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+const remove = async (req, res) => {
+	try {
+		const id = req.params.id;	
+
+		await prisma.reaction.delete({where: {id}});
+
+		return messages.deletedSuccessfully(res, "Reaction deleted Successfully");
+
+	} catch (error) {
+		console.error("Deleting reaction error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+module.exports = {
+	create,
+	retrieve,
+	update,
+	remove
+};
---
+++ controllers/report.js
@@ -0,0 +1,75 @@
+const prisma = require("../config/connection");
+const messages = require("../helper/messages");
+
+const create = async (req, res) => {
+	try {
+		const post_id = req.params.post_id;	
+		const user_id = req.session.user_id;
+		const { reason } = req.body;	
+
+		const report = await prisma.report.create({
+			data: {
+				postId: post_id,
+				userId: user_id,
+				reason, 
+			}
+		});
+
+		return messages.createdSuccessfully(res, "Report Saved Successfully");
+
+	} catch (error) {
+		console.error("Saving report error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+const retrieve = async (req, res) => {
+	try {
+		const report = req.data;
+
+		res.json(report);
+
+	} catch (error) {
+		console.error("Retrieving report error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+const update = async (req, res) => {
+	try {
+		//const comment = req.params.id;	
+
+		return messages.notImplemented(res);
+
+		//await prisma.report.update();
+		
+	} catch (error) {
+		console.error("Updating report error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+const remove = async (req, res) => {
+	try {
+		const id = req.params.id;	
+
+		await prisma.report.delete({where: {id}});
+
+		return messages.deletedSuccessfully(res, "Report deleted Successfully");
+
+	} catch (error) {
+		console.error("Deleting report error: ", error);
+
+		return messages.serverError(res);
+	}
+};
+
+module.exports = {
+	create,
+	retrieve,
+	update,
+	remove
+};
